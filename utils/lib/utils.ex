02dfe669c225cbc36c01ab15d91ea8d9b12a8447
Brooklin <brooklinjazzmyers@gmail.com>
2022-03-08 12:49:15 -0800

    extract TODO - Hide values out of mazez, naming numbers, comments, and boolean_operators

--- utils/lib/utils.ex
+++ utils/lib/utils.ex
@@ -46,7 +46,77 @@ defmodule Utils do
   end
 
   @doc ~S"""
+  iex> %{"west" => _} = Utils.get(:string_maze)
+  iex> %{south: _} = Utils.get(:atom_maze)
+  """
+  def get(:string_maze) do
+    put_in(
+      %{},
+      Enum.map(
+        [
+          "west",
+          "south",
+          "east",
+          "north",
+          "east",
+          "south",
+          "east",
+          "south",
+          "east",
+          "south",
+          "west",
+          "north"
+        ],
+        &Access.key(&1, %{})
+      ),
+      "Exit!"
+    )
+  end
+
+  def get(:atom_maze) do
+    put_in(
+      %{},
+      Enum.map(
+        [
+          :south,
+          :east,
+          :north,
+          :east,
+          :south,
+          :west,
+          :north,
+          :east,
+          :south,
+          :east,
+          :north,
+          :west,
+          :south,
+          :west,
+          :south,
+          :west,
+          :north,
+          :west,
+          :south,
+          :west,
+          :south,
+          :west,
+          :south,
+          :east,
+          :north,
+          :east,
+          :south,
+          :east,
+          :south
+        ],
+        &Access.key(&1, %{})
+      ),
+      "Exit!"
+    )
+  end
+
+  @doc ~S"""
   iex> :ok = Utils.graph(:binary_search)
+  iex> :ok = Utils.graph(:comprehension_big_o)
   """
   def graph(:binary_search) do
     size = 600
@@ -67,6 +137,69 @@ defmodule Utils do
     logn = for n <- init..max, do: %{"number of elements": n, time: :math.log2(n), type: "log(n)"}
 
     Kino.VegaLite.push_many(widget, logn)
+  end
+
+  def graph(:comprehension_big_o) do
+    size = 600
+
+    widget =
+      VegaLite.new(width: size, height: size)
+      |> VegaLite.mark(:line)
+      |> VegaLite.encode_field(:x, "number of elements", type: :quantitative)
+      |> VegaLite.encode_field(:y, "time", type: :quantitative)
+      |> VegaLite.transform(groupby: ["color"], extent: [2500, 6500])
+      |> VegaLite.encode_field(:color, "type", title: "Number Of Generators", type: :nominal)
+      |> Kino.VegaLite.new()
+      |> Kino.render()
+
+    init = 1
+    max = 5
+
+    n1 = for n <- init..max, do: %{"number of elements": n, time: n, type: "1"}
+    n2 = for n <- init..max, do: %{"number of elements": n, time: n ** 2, type: "2"}
+    n3 = for n <- init..max, do: %{"number of elements": n, time: n ** 3, type: "3"}
+    n4 = for n <- init..max, do: %{"number of elements": n, time: n ** 4, type: "4"}
+
+    Kino.VegaLite.push_many(widget, n1)
+    Kino.VegaLite.push_many(widget, n2)
+    Kino.VegaLite.push_many(widget, n3)
+    Kino.VegaLite.push_many(widget, n4)
+  end
+
+  @doc ~S"""
+  iex> Utils.test(:naming_numbers, fn int -> Enum.at(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], int) end)
+  """
+  def test(:naming_numbers, convert_to_named_integer) do
+    ExUnit.start(auto_run: false)
+
+    :persistent_term.put(:convert_to_named_integer, convert_to_named_integer)
+
+    defmodule NamingNumbers do
+      use ExUnit.Case
+
+      test "convert_to_named_integer" do
+        answer_key = [
+          {0, "zero"},
+          {1, "one"},
+          {2, "two"},
+          {3, "three"},
+          {4, "four"},
+          {5, "five"},
+          {6, "six"},
+          {7, "seven"},
+          {8, "eight"},
+          {9, "nine"}
+        ]
+
+        convert_to_named_integer = :persistent_term.get(:convert_to_named_integer)
+
+        Enum.each(answer_key, fn {key, value} ->
+          assert convert_to_named_integer.(key) === value
+        end)
+      end
+    end
+
+    ExUnit.run()
   end
 
   @doc ~S"""
