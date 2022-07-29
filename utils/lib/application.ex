98cc858db6a7fe8bc4190c8a1468b8a634a462ae
Mraiih <gael.roussel@shopmium.com>
2022-07-29 13:39:11 +0200

    removed TestedCell from Utils ; add TestedCell from other repo in dependencies

--- utils/lib/application.ex
+++ utils/lib/application.ex
@@ -8,7 +8,6 @@ defmodule Utils.Application do
   def start(_type, _args) do
     Enum.each(Utils.SmartCell.Exercise.smart_cells(), &Kino.SmartCell.register/1)
     Kino.SmartCell.register(Utils.SmartCell.HiddenCell)
-    Kino.SmartCell.register(Utils.SmartCell.TestedCell)
     children = []
     Supervisor.start_link(children, strategy: :one_for_one)
   end
