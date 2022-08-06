8425debd2cfa4bf265a2a56d75f75fcea8f96da0
Mraiih <gael.roussel@shopmium.com>
2022-08-06 21:05:14 +0200

    Voter Count exercises: Replace Utils.feedback with TestedCell

--- utils/lib/solutions.ex
+++ utils/lib/solutions.ex
@@ -182,16 +182,6 @@ defmodule Utils.Solutions do
     FizzBuzz
   end
 
-  defmodule VoterCount do
-    def count(list_of_votes, vote) do
-      Enum.count(list_of_votes, fn each -> each == vote end)
-    end
-  end
-
-  def voter_count do
-    VoterCount
-  end
-
   defmodule Anagram do
     def sort_string(string) do
       String.split(string, "", trim: true) |> Enum.sort() |> Enum.join()
