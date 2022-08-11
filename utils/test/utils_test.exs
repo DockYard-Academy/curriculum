9910975be41599b27e46358c6ee2efec48756444
Miguel Bermudez <miguel@miguelbermudez.com>
2022-08-11 13:44:18 -0400

    move test into utils_test and refactor

--- utils/test/utils_test.exs
+++ utils/test/utils_test.exs
@@ -3,6 +3,7 @@ defmodule UtilsTest do
   doctest Utils
   alias Utils.Factory
   alias Utils.Solutions
+  alias Livebook.LiveMarkdown.MarkdownHelpers
 
   test "slide/1" do
     Utils.Slide.__info__(:functions)
@@ -89,6 +90,50 @@ defmodule UtilsTest do
     refute file_contains?(~r/TestedCell\./, exercises ++ reading)
   end
 
+  describe "Notebook Headlines" do
+    test "h2-h6 titles should be in title case" do
+      one_word_title_titlecase_regex = ~r/^[A-Z].*$/
+
+      exercises = fetch_livebooks("../exercises/")
+      reading = fetch_livebooks("../reading/")
+
+      (exercises ++ reading)
+      |> Enum.each(fn filename ->
+        {_, ast, _} = filename |> File.read!() |> MarkdownHelpers.markdown_to_block_ast()
+
+        # get only headers in file
+        header_list =
+          Enum.filter(ast, fn
+            # only match h2 - h6 since h1's are notebook titles
+            {tag, _, _, _} when is_binary(tag) -> String.match?(tag, ~r/h[2-6]/)
+            _ -> false
+          end)
+
+        # check each header for titlecase
+        Enum.each(header_list, fn {_tag, _, [content], _} ->
+          word_list = String.split(content)
+          single_word = word_list |> filter_headline |> List.first()
+
+          # if header is only one word, check that it's capitalized
+          if length(word_list) == 1 && is_binary(single_word) &&
+               !String.starts_with?(single_word, ":") do
+            assert String.match?(single_word, one_word_title_titlecase_regex),
+                   "[#{filename}] expected: \"#{single_word}\" to be capitalized"
+          else
+            is_line_titlecase =
+              word_list
+              # filter out punctuations , `#`'s, articles, conjunctions, prepositions, numbers
+              |> filter_headline
+              # check each word starts with a capital letter
+              |> Enum.all?(fn word -> String.match?(word, ~r/^[A-Z]/) end)
+
+            assert is_line_titlecase, "[#{filename}] expected \"#{content}\" to be titlecase"
+          end
+        end)
+      end)
+    end
+  end
+
   defp file_contains?(regex, livebooks) do
     livebooks
     |> Stream.map(fn file ->
@@ -102,5 +147,21 @@ defmodule UtilsTest do
     File.ls!(path)
     |> Stream.filter(&String.ends_with?(&1, ".livemd"))
     |> Enum.map(&(path <> &1))
+  end
+
+  defp filter_headline(word_list) do
+    Enum.filter(
+      word_list,
+      &(!String.match?(
+          &1,
+          # ~r/
+          # method_names: e.g. increased/1, Stream.iterate/2, <start_link> |
+          # all punctuations |
+          # hashes |
+          # word-boundary numbers word-boundary
+          # /
+          ~r/.*\/[0-9]$|[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]|#+|\b[0-9]+\b/
+        ))
+    )
   end
 end
