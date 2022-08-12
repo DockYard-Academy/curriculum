93e81f5a230f2b1082895d3014787089e1af90f9
Marko Vukovic <mvkvc.git@gmail.com>
2022-08-12 13:12:59 -0400

    Cleanup

--- utils/add_nav.exs
+++ utils/add_nav.exs
@@ -31,6 +31,20 @@ defmodule AddNav do
   def edit_file(filepath) do
     # Check if last two lines are newline/nav line
     # If so overwrite, if not append
+    # """
+    # <span align="left;">
+    #     PREVIOUS
+    #     <span style="float:right;">
+    #         NEXT
+    #     </span>
+    # </p>
+    # <span align="left;">
+    #     <a href="../reading/#{prev_link}">Prev Link</a>
+    #     <span style="float:right;">
+    #         <a href="#{next_link}">Next Link</a>
+    #     </span>
+    # </p>
+    # """
   end
 
   def run(folder) do
@@ -40,37 +54,4 @@ end
 
 start_path = Path.expand("../start.livemd")
 start = AddNav.get_readings(start_path)
-# IO.inspect(start)
-
-text1 = AddNav.get_prev_next(start, "command_line.livemd")
-text2 = AddNav.get_prev_next(start, "match_operator.livemd")
-text3 = AddNav.get_prev_next(start, "book_search_tags.livemd")
-# IO.inspect([text1, text2, text3])
-
-# for file <- start do
-#   IO.puts(file)
-# end
-
-def link_to_title(filename) do
-  # filename
-  #   |> TRIM .LIVEMD
-  #   |> REPLACE UNDERSCORE WITH SPACE
-  #   |> CAPITALIZE ALL FIRST LETTER OF WORDS
-end
-
-prev_link = "apis.livemd"
-next_link = "comments.livemd"
-links_insert = """
-<span align="left;">
-    PREVIOUS
-    <span style="float:right;">
-        NEXT
-    </span>
-</p>
-<span align="left;">
-    <a href="../reading/#{prev_link}">Prev Link</a>
-    <span style="float:right;">
-        <a href="#{next_link}">Next Link</a>
-    </span>
-</p>
-"""
+IO.inspect(start)
