5b5d7d09a9aa687f734187eff62e3613708c3dee
Brooklin <brooklinjazzmyers@gmail.com>
2022-09-21 21:39:55 -0700

    remove Utils.visual

--- utils/lib/visual.ex
+++
@@ -1,6 +0,0 @@
-defmodule Utils.Visual do
-  def visual(:light_control, power) do
-    content = if power, do: "/images/on.png", else: "/images/off.png"
-    Kino.Image.new(File.read!(__DIR__ <> content), :png)
-  end
-end
