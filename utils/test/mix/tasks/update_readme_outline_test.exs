1043495425b3797d5d598b2cc42e700b77caad9a
Brooklin <brooklinjazzmyers@gmail.com>
2022-10-12 23:27:34 -0700

    format elixir code

--- utils/test/mix/tasks/update_readme_outline_test.exs
+++ utils/test/mix/tasks/update_readme_outline_test.exs
@@ -21,7 +21,10 @@ defmodule Mix.Tasks.Bc.UpdateReadmeOutlineTest do
     [start, outline, finish] = UpdateReadmeOutline.split_on_outline(readme)
 
     assert start == "start\n"
-    assert outline == "\n## Section 1\n### SubSection 2\n* Reading\n  * Topic\n* Exercises\n  * Exercise\n### SubSection 3\n"
+
+    assert outline ==
+             "\n## Section 1\n### SubSection 2\n* Reading\n  * Topic\n* Exercises\n  * Exercise\n### SubSection 3\n"
+
     assert finish == "\nfinish\n"
   end
 
@@ -38,9 +41,9 @@ defmodule Mix.Tasks.Bc.UpdateReadmeOutlineTest do
     """
 
     assert UpdateReadmeOutline.sections(outline) == """
-    ## Section 1
-    * SubSection 2
-    * SubSection 3
-    """
+           ## Section 1
+           * SubSection 2
+           * SubSection 3
+           """
   end
 end
