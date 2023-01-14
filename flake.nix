87da001153de189e76949b1c3d31746534b98c05
Marko Vukovic <mvkvc.git@gmail.com>
2023-01-14 15:45:36 -0500

    Update nix files to use specific elixir/erlang versions and install livebook automatically

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
-  description = "DockYard academy";
+  description = "DockYard Academy";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        installScript = pkgs.writeShellScriptBin "installScript" ''
+        if [ ! -f $MIX_HOME/escripts/livebook ]; then
+          mix do local.rebar --force --if-missing
+          mix do local.hex --force --if-missing
+          mix escript.install --force hex livebook
+        fi
+        '';
+      in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            erlang
-            elixir
+            installScript
+            beam.interpreters.erlangR25
+            beam.packages.erlangR25.elixir_1_14
           ];
           shellHook = ''
-              mkdir -p .nix_mix
-              mkdir -p .nix_hex
-              export MIX_HOME=$PWD/utils/.nix_mix
-              export HEX_HOME=$PWD/utils/.nix_hex
+              export MIX_HOME=$PWD/.nix_mix
+              export HEX_HOME=$PWD/.nix_hex
               export PATH=$MIX_HOME/bin:$PATH
               export PATH=$HEX_HOME/bin:$PATH
               export PATH=$MIX_HOME/escripts:$PATH
               export LIVEBOOK_HOME=$PWD
+              mkdir -p .nix_mix
+              mkdir -p .nix_hex
+              installScript
             '';
         };
       }
