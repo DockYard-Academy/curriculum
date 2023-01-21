337715faaa0d25417e739301bc818f22bb91112f
Brooklin <brooklinjazzmyers@gmail.com>
2023-01-20 21:38:12 -0800

    deprecate unused files

--- utils/test/utils_test.exs
+++ utils/test/utils_test.exs
@@ -98,6 +98,34 @@ defmodule UtilsTest do
     end)
   end
 
+  test "Ensure fileu not in outline are deprecated" do
+    outline = File.read!("../start.livemd")
+
+    all_paths = Path.wildcard("../*/*.livemd")
+    ignored_paths = Path.wildcard("../*/_*.livemd")
+    deprecated_paths = Path.wildcard("../*/deprecated*.livemd")
+
+    outline_paths =
+      Regex.scan(~r/\[[^\]]+\]\(([^\)]+\.livemd)\)/, outline)
+      |> Enum.map(fn [_, file_name] ->
+        Path.join("../", file_name)
+      end)
+
+    remaining_paths = all_paths -- ignored_paths
+    remaining_paths = remaining_paths -- deprecated_paths
+    remaining_paths = remaining_paths -- outline_paths
+    assert remaining_paths == []
+
+    # Enum.each(remaining_paths, fn path ->
+    #   basename = Path.basename(path)
+    #   dirname = Path.dirname(path)
+
+    #   deprecated_path = Path.join(dirname, "deprecated_" <> basename)
+    #   File.cp!(path, deprecated_path)
+    #   File.rm!(path)
+    # end)
+  end
+
   test "Ensure no broken / empty links in livebooks" do
     Notebooks.stream_lines(Notebooks.reading() ++ Notebooks.exercises(), fn line, file_name ->
       # Empty Links
