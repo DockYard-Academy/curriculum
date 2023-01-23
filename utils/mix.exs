0b3cf876cb5a7b34864f87932003e796222f1fc0
Brooklin <brooklinjazzmyers@gmail.com>
2023-01-22 23:47:31 -0800

    remove floating code in mix.exs

--- utils/mix.exs
+++ utils/mix.exs
@@ -18,8 +18,7 @@ defmodule Utils.MixProject do
       mod: {Utils.Application, []}
     ]
   end
-game = %{team1_name: "Team Team", team2_name: "Exersport", round1: %{team1: 10, team2: 20}, round2: %{team1: 30, team2: 10}, round3: %{team1: 25, team2: 30}}
-  # Run "mix help deps" to learn about dependencies.
+
   defp deps do
     [
       {:credo, "~> 1.6", runtime: false, only: [:dev, :test]},
