d5f62d5a3add808d0a538c8a7a25942466a5d1ee
Matt Humphrey <mhumphrey9@hotmail.com>
2023-03-01 15:32:23 +0800

    finish exdoc reading

--- math/mix.exs
+++ math/mix.exs
@@ -21,7 +21,8 @@ defmodule Math.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
