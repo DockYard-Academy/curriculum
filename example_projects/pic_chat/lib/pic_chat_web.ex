a2716ec64cf394d6fd52bceed6c7b2527b6d666b
Brooklin <brooklinjazzmyers@gmail.com>
2023-05-29 22:34:27 -0700

    Add navigation to picchat and fix pic_chat uploads reloading due to being in the images folder.

--- example_projects/pic_chat/lib/pic_chat_web.ex
+++ example_projects/pic_chat/lib/pic_chat_web.ex
@@ -17,7 +17,7 @@ defmodule PicChatWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets uploads fonts images favicon.ico robots.txt)
 
   def router do
     quote do
