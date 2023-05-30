6c023139325949d014011e5ced1c673bca415f1b
Brooklin <brooklinjazzmyers@gmail.com>
2023-05-29 21:35:18 -0700

    format picchat and add .env to .gitignore instructions in reading material

--- example_projects/pic_chat/lib/pic_chat/summary_email.ex
+++ example_projects/pic_chat/lib/pic_chat/summary_email.ex
@@ -1,31 +1,32 @@
 defmodule PicChat.SummaryEmail do
   import Swoosh.Email
 
-  @sender_name "Brooklin"
-  @sender_email "brooklin@elixirnewbie.com"
+  @sender_name "sender"
+  @sender_email "sender@email.com"
 
   def send_to_subscribers do
     messages = PicChat.Chat.todays_messages()
     subscribers = PicChat.Accounts.subscriber_emails()
 
     for subscriber <- subscribers do
-      PicChat.Mailer.deliver(      build(subscriber, messages))
+      PicChat.Mailer.deliver(build(subscriber, messages))
     end
   end
 
   def build(receiver_email, messages) do
-    email = new()
-    |> to(receiver_email)
-    |> from({@sender_name, @sender_email})
-    |> subject("PicChat Summary Report")
-    |> html_body("""
-    <h1>Summary Report</h1>
-    #{Enum.map(messages, &render_message/1)}
-    """)
-    |> text_body("""
-    Summary Report
-    #{messages |> Enum.map(&(&1.content)) |> Enum.join("\n")}
-    """)
+    email =
+      new()
+      |> to(receiver_email)
+      |> from({@sender_name, @sender_email})
+      |> subject("PicChat Summary Report")
+      |> html_body("""
+      <h1>Summary Report</h1>
+      #{Enum.map(messages, &render_message/1)}
+      """)
+      |> text_body("""
+      Summary Report
+      #{messages |> Enum.map(& &1.content) |> Enum.join("\n")}
+      """)
   end
 
   defp render_message(message) do
