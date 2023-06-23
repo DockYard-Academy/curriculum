589c8e6ffd6c52035cb2d2369db9480545472140
Patrick <patrick@pop-os.localdomain>
2023-06-23 16:06:16 -0700

    added map_notebook function
    map_notebook function will take a list of maps and convert it into a list of notebook structs.

--- utils/lib/notebooks/notebook.ex
+++ utils/lib/notebooks/notebook.ex
@@ -27,13 +27,39 @@ defmodule Utils.Notebooks.Notebook do
   require Logger
 
   @doc """
+  Given a list of names and relative paths will create a list of notebook structs
+  with the next_notebook and prev_notebook attributes set according to the position 
+  of the notebook in the list.
+
+  Useful for parsing input that is read from the start.livemd file.
+  """
+  @spec map_notebooks([%{name: String.t(), relative_path: String.t()}]) :: [__MODULE__.t()]
+  def map_notebooks(nodes) do
+    [first, second | _rest] = nodes
+    first_node = Map.put(first, :next_notebook, second) |> Map.put(:prev_notebook, %{})
+
+    rest =
+      Enum.chunk_every(nodes, 3, 1)
+      |> Enum.map(fn
+        [prev, curr, next] ->
+          Map.put(curr, :prev_notebook, prev)
+          |> Map.put(:next_notebook, next)
+
+        [prev, curr] ->
+          Map.put(curr, :prev_notebook, prev)
+          |> Map.put(:next_notebook, %{})
+      end)
+
+    [first_node | rest]
+    |> Enum.map(&new!/1)
+  end
+
+  @doc """
   Create a new Notebook struct
 
   iex> Utils.Notebooks.Notebook.new!(%{index: 0, relative_path: "../reading/strings_and_binaries.livemd"})
   %Utils.Notebooks.Notebook{index: 0, relative_path: "../reading/strings_and_binaries.livemd", name: "strings_and_binaries", title: "Strings And Binaries", folder: "reading", type: :reading}
   """
-  require Logger
-
   def new!(%{relative_path: "../start.livemd"} = attrs) do
     struct!(
       __MODULE__,
